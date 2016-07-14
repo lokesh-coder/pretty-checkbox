4b495e45ec269fbf59b5d9580c12d2dc553fc7c7
lokesh-coder <mexican.dirtyfellow@gmail.com>
2016-07-14 16:30:53 +0530

    Document example codes added

---
+++ bower_components/pretty-checkbox/Gulpfile.js
@@ -0,0 +1,43 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var reload = browserSync.reload;
+
+module.exports = gulp;
+
+gulp.task('browser-sync', function() {
+  browserSync({
+    port: 3040,
+    server: {
+      baseDir: "./",
+      directory: true
+    }
+  });
+});
+
+gulp.task('sass', function() {
+  return gulp.src('src/**/*.scss')
+    .pipe(sass({
+      outputStyle: 'expanded'
+    }).on('error', sass.logError))
+    .pipe(gulp.dest('src/'))
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('src/'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+gulp.task('bs-reload', function() {
+  browserSync.reload();
+});
+
+gulp.task('default', ['sass', 'browser-sync'], function() {
+  gulp.watch("src/**/*.scss", ['sass', 'bs-reload']);
+});
