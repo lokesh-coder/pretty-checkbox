624b25249221b96797a1db3260673d1c916f00ea
lokesh_coder <mexican.dirtyfellow@gmail.com>
2017-10-15 23:29:52 -0700

    refactor

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,6 +3,8 @@ var browserSync = require('browser-sync');
 var sass = require('gulp-sass');
 var rename = require('gulp-rename');
 var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var headerComment = require('gulp-header-comment');
 var reload = browserSync.reload;
 
 module.exports = gulp;
@@ -17,8 +19,9 @@ gulp.task('browser-sync', function() {
   });
 });
 
-gulp.task('sass', function() {
+gulp.task('build:sass', function() {
   return gulp.src('src/**/*.scss')
+   .pipe(sourcemaps.init())
     .pipe(sass({
       outputStyle: 'expanded'
     })
@@ -34,16 +37,30 @@ gulp.task('sass', function() {
     .pipe(rename({
       suffix: '.min'
     }))
+    .pipe(sourcemaps.write('./maps'))
     .pipe(gulp.dest('dist'))
     .pipe(browserSync.reload({
       stream: true
     }));
 });
 
+gulp.task('header:comment',function(){
+  return gulp.src('dist/**/*.css')
+    .pipe(headerComment(`
+      pretty-checkbox.css - <%= pkg.homepage %>
+
+      Version: v<%= pkg.version %>
+      Licensed under the MIT license - http://opensource.org/licenses/MIT
+
+      Copyright (c) <%= moment().format('YYYY') %> <%= _.capitalize(pkg.author) %>
+    `))
+    .pipe(gulp.dest('./dist/'))
+});
+
 gulp.task('bs-reload', function() {
   browserSync.reload();
 });
 
-gulp.task('default', ['sass', 'browser-sync'], function() {
-  gulp.watch("src/**/*.scss", ['sass', 'bs-reload']);
+gulp.task('default', ['build:sass', 'browser-sync'], function () {
+  gulp.watch("src/**/*.scss", ['build:sass', 'bs-reload']);
 });
