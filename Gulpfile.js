6c3aa71055b774dbf7cbbae5cc906636284b89a5
lokesh_coder <mexican.dirtyfellow@gmail.com>
2017-10-21 04:24:54 -0700

    ci build set up updated

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,9 +5,16 @@ var rename = require('gulp-rename');
 var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
 var headerComment = require('gulp-header-comment');
+const gulpStylelint = require('gulp-stylelint');
+var stylefmt = require('gulp-stylefmt');
+let cleanCSS = require('gulp-clean-css');
+var gulpSequence = require('gulp-sequence')
+var del = require('del');
 var reload = browserSync.reload;
 
 module.exports = gulp;
+
+/* BROWSER SYNC */
 
 gulp.task('browser-sync', function () {
   browserSync({
@@ -16,27 +23,37 @@ gulp.task('browser-sync', function () {
       baseDir: "./",
       directory: true
     },
-    // https: true
+    https: true
   });
 });
 
-gulp.task('build:sass', function () {
+gulp.task('browser-sync-reload', function () {
+  browserSync.reload();
+});
+
+/* LIST SCSS */
+gulp.task('lint:scss', function() {
+  return gulp
+    .src('src/**/*.scss')
+    .pipe(gulpStylelint({
+      reporters: [
+        { formatter: 'string', console: true }
+      ]
+    }));
+});
+
+
+/* COMPILE SCSS */
+gulp.task('compile:scss', function () {
   return gulp.src('src/**/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass({
-        outputStyle: 'expanded'
-      })
+      outputStyle: 'expanded'
+    })
       .on('error', sass.logError))
     .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
+      browsers: ['> 5%', 'last 4 versions'],
       cascade: false
-    }))
-    .pipe(gulp.dest('dist'))
-    .pipe(sass({
-      outputStyle: 'compressed'
-    }))
-    .pipe(rename({
-      suffix: '.min'
     }))
     .pipe(sourcemaps.write('./maps'))
     .pipe(gulp.dest('dist'))
@@ -45,25 +62,55 @@ gulp.task('build:sass', function () {
     }));
 });
 
+/* FORMAT CSS */
 
-gulp.task('append:header', function () {
-  return gulp.src('dist/**/*.css')
+gulp.task('format:css', function () {
+  return gulp.src('dist/*.css')
+    .pipe(stylefmt())
+    .pipe(gulp.dest('dist'));
+})
+
+/* CLEAN DIST */
+gulp.task('clean:dist', function () {
+  return del(['dist']);
+});
+
+/* MINIFY CSS */
+gulp.task('minify:css', () => {
+  return gulp.src('dist/*.css')
+    .pipe(cleanCSS({ compatibility: 'ie9' }))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
+/* SET HEADER */
+
+gulp.task('set:header', function () {
+  return gulp.src('dist/*.css')
     .pipe(headerComment(`
-      pretty-checkbox.css - <%= pkg.homepage %>
+      pretty-checkbox.css
 
-      Version: v<%= pkg.version %>
-      Licensed under the MIT license - http://opensource.org/licenses/MIT
+      A pure CSS library to beautify checkbox and radio buttons.
+
+      Source: <%= pkg.repository.link %>
+      Demo: <%= pkg.homepage %>
 
       Copyright (c) <%= moment().format('YYYY') %> <%= _.capitalize(pkg.author) %>
     `))
-    .pipe(gulp.dest('./dist/'))
+    .pipe(gulp.dest('dist'))
 });
 
 
-gulp.task('bs-reload', function () {
-  browserSync.reload();
-});
+
 
 gulp.task('default', ['build:sass', 'browser-sync'], function () {
-  gulp.watch("src/**/*.scss", ['build:sass', 'bs-reload']);
+  gulp.watch("src/**/*.scss", ['build:sass', 'browser-sync-reload']);
 });
+
+gulp.task('build', function (cb) {
+  gulpSequence('lint:scss','clean:dist', 'compile:scss', 'format:css', 'minify:css', 'set:header', cb)
+});
+
+
