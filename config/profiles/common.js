ea2effb41573e67f49191e9ae34eb372d0288a10
lokesh_coder <mexican.dirtyfellow@gmail.com>
2018-08-26 18:56:56 +0530

    feat: added default check and cross icons

--- config/profiles/common.js
+++ config/profiles/common.js
@@ -34,6 +34,18 @@ module.exports = merge({
             options: {}
           }
         ]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
       }
     ]
   },
